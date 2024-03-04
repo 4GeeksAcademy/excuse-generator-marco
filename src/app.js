23a7e9905cfc3035eff832fd7c9a8bd7877f39d7
marcoluqueh <101642586+marcoluqueh@users.noreply.github.com>
2024-03-04 15:50:51 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let nuevaExcusa =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+
+  document.getElementById("excuse").innerHTML = nuevaExcusa;
+}
+
+window.onload = getRandomExcuse;
